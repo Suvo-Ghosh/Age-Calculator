c631524db56642c973ad7c9b29431151f4d76d8b
Suvo Ghosh <suvosh663@gmail.com>
2024-11-12 00:15:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Age Calculator
+
+A simple web app to calculate your age in years, months, and days based on your date of birth.
+
+## Features
+
+- **Enter your Date of Birth**: Use the date picker to select your birthdate.
+- **Calculate Age**: Click the "Calculate" button to calculate your age in years, months, and days.
+- **Display Result**: The app shows your age in a readable format.
+
+## Usage
+
+1. Select your **Date of Birth** using the date picker.
+2. Click the **"Calculate"** button to see your age.
+3. The result will display your age in years, months, and days.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+##
+Happy Codding 🎉
---
+++ script.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('button')
+const inputDate = document.querySelector('.input-btn input')
+const result = document.querySelector('p')
+
+btn.addEventListener('click', () => {
+    const dob = new Date(inputDate.value)
+    const currentDate = new Date()
+
+    const barthDetails = {
+        date: dob.getDate(),
+        month: dob.getMonth() + 1,
+        year: dob.getFullYear()
+    }
+    const currentDateDetails = {
+        date: currentDate.getDate(),
+        month: currentDate.getMonth() + 1,
+        year: currentDate.getFullYear()
+    }
+    if (dob.value = '') {
+        alert('Enter Your DOB')
+    }
+    yearDiff = currentDateDetails.year - barthDetails.year
+
+    if (currentDateDetails.month >= barthDetails.month) {
+        monDiff = currentDateDetails.month - barthDetails.month
+    } else {
+        yearDiff--
+        monDiff = 12 + currentDateDetails.month - barthDetails.month
+    }
+
+    if (currentDateDetails.date >= barthDetails.date) {
+        dayDiff = currentDateDetails.date - barthDetails.date
+    } else {
+        monDiff--
+        lastMonthDay = (year, month) => { return new Date(year, month, 0).getDate() }
+        dayDiff = lastMonthDay(barthDetails.year, barthDetails.month) + currentDateDetails.date - barthDetails.date
+    }
+
+    if (monDiff < 0) {
+        yearDiff--
+        monDiff = 11
+    }
+    console.log(yearDiff, monDiff, dayDiff)
+    result.innerHTML = `Your age is <span>${yearDiff}</span> Year , <span>${monDiff}</span> months and <span>${dayDiff}</span> days`
+})
